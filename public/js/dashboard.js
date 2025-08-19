ab54889ed0f6817df06ceaf1e6ed986367f06f10
Loukya Sri <loukyasrikudipudi@gmail.com>
2025-08-19 13:56:13 +0530

    add crypto bot

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -18,7 +18,20 @@ let searchQuery = "";
 // ---------------------
 // Auth check
 // ---------------------
-if (!token) {
+
+function isTokenValid(token) {
+  if (!token) return false;
+
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const now = Math.floor(Date.now() / 1000); // current time in seconds
+    return payload.exp > now;
+  } catch (err) {
+    return false;
+  }
+}
+
+if (!isTokenValid(token)) {
   window.location.href = "/login.html";
 }
 
